745c92f21f9d3ad76ebae18107553d705650da7c
joodle <joodle@joodle.nl>
2015-02-07 15:39:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,26 +15,31 @@ fi
   clear
 }
 
-# Updating + Upgrading the system, installing MariaDB Server + Hiawatha + PHP5-FPM
-apt-get update && apt-get upgrade -y;apt-get install python-software-properties -y;apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db && add-apt-repository 'deb http://nwps.ws/pub/mariadb/repo/5.5/debian wheezy main';apt-get update;apt-get install mariadb-server -y;apt-get install libxslt1.1;wget http://files.tuxhelp.org/hiawatha/hiawatha_9.11-1_amd64.deb;dpkg -i hiawatha_9.11-1_amd64.deb;apt-get install php5 php-pear php5-common php5-curl php5-mysql php5-fpm php5-gd -y;apt-get install unzip -y;;
+# Updating + Upgrading the system and adding repository
+apt-get update && apt-get upgrade -y;apt-get install python-software-properties -y ;;
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db && add-apt-repository 'deb http://nwps.ws/pub/mariadb/repo/5.5/debian wheezy main' ;;
+apt-get update ;;
+
+# Installing MariaDB Server + Hiawatha + PHP5-FPM
+apt-get install mariadb-server -y;apt-get install libxslt1.1;wget http://files.tuxhelp.org/hiawatha/hiawatha_9.11-1_amd64.deb;dpkg -i hiawatha_9.11-1_amd64.deb;apt-get install php5 php-pear php5-common php5-curl php5-mysql php5-fpm php5-gd -y;apt-get install unzip -y;;
 
 # We're going to download the PHP-FPM php.ini file now
-rm /etc/php5/fpm/php.ini;wget --no-check-certificate -O /etc/php5/fpm/php.ini https://raw.githubusercontent.com/joodle/Hiawatha-MariaDB-PHP5-FPM-SQLBuddy/master/php.ini;;
+rm /etc/php5/fpm/php.ini;wget --no-check-certificate -O /etc/php5/fpm/php.ini https://raw.githubusercontent.com/joodle/Hiawatha-MariaDB-PHP5-FPM-SQLBuddy/master/php.ini ;;
 
 # We're going to download the PHP-FPM config file (php-fpm.conf) now
-rm /etc/php5/fpm/php-fpm.conf;wget --no-check-certificate -O /etc/php5/fpm/php-fpm.conf https://raw.githubusercontent.com/joodle/Hiawatha-MariaDB-PHP5-FPM-SQLBuddy/master/php-fpm.conf;;
+rm /etc/php5/fpm/php-fpm.conf;wget --no-check-certificate -O /etc/php5/fpm/php-fpm.conf https://raw.githubusercontent.com/joodle/Hiawatha-MariaDB-PHP5-FPM-SQLBuddy/master/php-fpm.conf ;;
 
 # We're going to download the Hiawatha config file (hiawatha.conf) now
-rm /etc/hiawatha/hiawatha.conf;wget --no-check-certificate -O /etc/hiawatha/hiawatha.conf https://raw.githubusercontent.com/joodle/Hiawatha-MariaDB-PHP5-FPM-SQLBuddy/master/hiawatha.conf;;
+rm /etc/hiawatha/hiawatha.conf;wget --no-check-certificate -O /etc/hiawatha/hiawatha.conf https://raw.githubusercontent.com/joodle/Hiawatha-MariaDB-PHP5-FPM-SQLBuddy/master/hiawatha.conf ;;
 
 # Creating folder for the Hiawatha webserver
 mkdir /var/www/first-site;;
 
 # Downloading and unzipping SQLBuddy
-wget --no-check-certificate -O /var/www/first-site https://github.com/calvinlough/sqlbuddy/raw/gh-pages/sqlbuddy.zip;unzip sqlbuddy.zip;rm sqlbuddy.zip;;
+wget --no-check-certificate -O /var/www/first-site https://github.com/calvinlough/sqlbuddy/raw/gh-pages/sqlbuddy.zip;unzip sqlbuddy.zip;rm sqlbuddy.zip ;;
 
 # Restarting Hiawatha and PHP-FPM
-service hiawatha restart;service php5-fpm restart;;
+service hiawatha restart;service php5-fpm restart ;;
 
 
 function enter ()
